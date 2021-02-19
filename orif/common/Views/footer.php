8e54bcf38ba289817721c4f3b4f9d3fbb071de93
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2021-02-19 08:28:47 +0100

    add favicon in firefox tab

--- orif/common/Views/footer.php
+++ orif/common/Views/footer.php
@@ -1,2 +1,6 @@
+<script defer type="text/javascript">
+    let icolink=document.querySelector("link:first-of-type");
+    document.getElementsByTagName("head")[0].append(icolink);
+</script>
 </body>
 </html>
