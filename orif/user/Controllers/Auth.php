c2a05f5ab0bcc5a78c11922c63dd0948feff0dee
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2021-03-03 13:07:11 +0100

    Updated auth file to CI4

--- orif/user/Controllers/Auth.php
+++ orif/user/Controllers/Auth.php
@@ -8,6 +8,7 @@
  * @version     2.0
  */
 namespace User\Controllers;
+use \User\Models as models;
 class Auth extends \App\Controllers\BaseController {
     /**
      * Constructor
@@ -94,39 +95,38 @@ class Auth extends \App\Controllers\BaseController {
                 // Check fields validation rules
 
                 if ($this->validation->withRequest($this->request)->run() == true) {
-                    echo "<script>alert('salut')</script>";
-
+                    $user_model=new models\User_model();
                     $input = $this->request->getVar('username');
                     $password = $this->request->getvar('password');
-                    //$ismail = $this->user_model->check_password_email($input, $password);
-
-                    /*if ($ismail || $this->user_model->check_password_name($input, $password)) {
+                    $ismail = $user_model->check_password_email($input, $password);
+                    if ($ismail || $user_model->check_password_name($input, $password)) {
                         // Login success
                         $user = NULL;
                         // User is either logging in through an email or an username
                         // Even if an username is entered like an email, we're not grabbing it
                         if ($ismail) {
-                            $user = $this->user_model->with('user_type')
-                                                     ->get_by('email', $input);
+                            //$user = $user_model->with('user_type')
+                            //                         ->get_by('email', $input);
+                            $user = $user_model->getWhere(['email'=>$input])->getRow();
                         } else {
-                            $user = $this->user_model->with('user_type')
-                                                     ->get_by('username', $input);
+                            //$user = $user_model->with('user_type')
+                            //                         ->get_by('username', $input);
+                            $user = $user_model->getWhere(['username'=>$input])->getRow();
                         }
 
                         // Set session variables
                         $_SESSION['user_id'] = (int)$user->id;
                         $_SESSION['username'] = (string)$user->username;
-                        $_SESSION['user_access'] = (int)$user->user_type->access_level;
+                        $_SESSION['user_access'] = (int)$user_model->get_access_level($user);
                         $_SESSION['logged_in'] = (bool)true;
 
                         // Send the user to the redirection URL
-                        redirect($_SESSION['after_login_redirect']);
+                        return redirect()->to($_SESSION['after_login_redirect']);
 
                     } else {
                         // Login failed
-                        $this->session->set_flashdata('message-danger', lang('msg_err_invalid_password'));
+                        $this->session->setFlashdata('message-danger', lang('My_user_lang.msg_err_invalid_password'));
                     }
-                    */
                     $this->session->setFlashdata('message-danger', lang('My_user_lang.msg_err_invalid_password'));
                 }
             }
@@ -135,7 +135,7 @@ class Auth extends \App\Controllers\BaseController {
             $output = array('title' => lang('MY_user_lang.title_page_login'));
             $this->display_view('\User\auth\login', $output);
         } else {
-            redirect(base_url());
+            return redirect()->to(base_url());
         }
     }
 
@@ -151,7 +151,7 @@ class Auth extends \App\Controllers\BaseController {
         session_reset();
         session_unset();
 
-        redirect(base_url());
+        return redirect()->to(base_url());
     }
 
     /**
@@ -165,73 +165,56 @@ class Auth extends \App\Controllers\BaseController {
         if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
 
             // Check if the form has been submitted, else just display the form
-            if (!is_null($this->input->post('btn_change_password'))) {
+            if (!is_null($this->request->getVar('btn_change_password'))) {
                 $username = $_SESSION["username"];
 
-                // Define fields validation rules
-                $validation_rules = array(
-                    array(
-                        'field' => 'old_password',
-                        'label' => 'lang:field_old_password',
+                $validation_rules =[
+                    'old_password'=>[
+                        'label' => 'My_user_lang.field_old_password',
                         'rules' => 'trim|required|'
-                                 . 'min_length['.$this->config->item('password_min_length').']|'
-                                 . 'max_length['.$this->config->item('password_max_length').']|'
-                                 . 'callback_cb_old_password_check['.$username.']',
-                        'errors' => array(
-                            'cb_old_password_check' => lang('msg_err_invalid_old_password')
-                        )
-                    ),
-                    array(
-                        'field' => 'new_password',
-                        'label' => 'lang:field_new_password',
-                        'rules' => 'trim|required|'
-                                 . 'min_length['.$this->config->item('password_min_length').']|'
-                                 . 'max_length['.$this->config->item('password_max_length').']'
-                    ),
-                    array(
-                        'field' => 'confirm_password',
-                        'label' => 'lang:field_password_confirm',
-                        'rules' => 'trim|required|'
-                                 . 'min_length['.$this->config->item('password_min_length').']|'
-                                 . 'max_length['.$this->config->item('password_max_length').']|'
-                                 . 'matches[new_password]'
-                    )
-                );
-                $this->form_validation->set_rules($validation_rules);
+                            . 'min_length['.config("\User\Config\User_config")->password_min_length.']|'
+                            . 'max_length['.config("\User\Config\User_config")->password_max_length.']|'
+                            . 'old_password_check['.$username.']',
+                        'errors' => ['old_password_check' => lang('MY_user_lang.msg_err_invalid_old_password')]
+                    ],
+                    'new_password'=>[
+                        'label' =>'My_user_lang.field_new_password',
+                        'rules' =>'trim|required|'
+                            . 'min_length['.config("\User\Config\User_config")->password_min_length.']|'
+                            . 'max_length['.config("\User\Config\User_config")->password_max_length.']'
+                    ],
+                    'confirm_password'=>[
+                        'label' =>'My_user_lang.field_password_confirm',
+                        'rules' =>'trim|required|'
+                            . 'min_length['.config("\User\Config\User_config")->password_min_length.']|'
+                            . 'max_length['.config("\User\Config\User_config")->password_max_length.']|'
+                            . 'matches[new_password]'
+                    ]
+                ];
+                $this->validation->setRules($validation_rules);
 
                 // Check fields validation rules
-                if ($this->form_validation->run() == true) {
-                    $old_password = $this->input->post('old_password');
-                    $new_password = $this->input->post('new_password');
-                    $confirm_password = $this->input->post('confirm_password');
+                if ($this->validation->withRequest($this->request)->run() == true) {
+                    $old_password = $this->request->getVar('old_password');
+                    $new_password = $this->request->getVar('new_password');
+                    $confirm_password = $this->request->getVar('confirm_password');
 
-                    $this->load->model('user_model');
-                    $this->user_model->update($_SESSION['user_id'],
-                            array("password" => password_hash($new_password, $this->config->item('password_hash_algorithm'))));
+                    $user_model=new models\User_model();
+                    $user_model->update($_SESSION['user_id'],
+                            array("password" => password_hash($new_password, config("\User\Config\User_config")->password_hash_algorithm)));
 
                     // Send the user back to the site's root
-                    redirect(base_url());
+                    echo "echococo";
+                    return redirect()->to(base_url());
                 }
             }
 
             // Display the password change form
-            $output['title'] = $this->lang->line('page_password_change');
-            $this->display_view('user/auth/change_password', $output);
+            $output['title'] = lang('My_user_lang.page_password_change');
+            $this->display_view('\User\auth\change_password', $output);
         } else {
             // Access is not allowed
-            redirect('user/auth/login');
+            return redirect()->to('user/auth/login');
         }
-    }
-
-    /**
-     * Callback method for change_password validation rule
-     *
-     * @param string $pwd = The previous password
-     * @param string $user = The username
-     * @return boolean = Whether or not the combination is correct
-     */
-    public function cb_old_password_check($pwd, $user)
-    {
-        return $this->user_model->check_password_name($user, $pwd);
     }
 }
