7d52d2c5bd1c96d64599e243e8ddb1e9db749e2b
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2021-03-03 13:14:52 +0100

    Created model for User_type table

---
+++ orif/user/Models/User_type_model.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace User\Models;
+
+
+class User_type_model extends \CodeIgniter\Model
+{
+    protected $table='user_type';
+    protected $primaryKey='id';
+    protected $protectFields=['id'];
+
+}
