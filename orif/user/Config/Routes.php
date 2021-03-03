cb3ed1b4a0dc13c91436dd1762270a9fd527a094
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2021-03-03 13:12:42 +0100

    Adding routes to user/auth/. to logout and change password

--- orif/user/Config/Routes.php
+++ orif/user/Config/Routes.php
@@ -2,6 +2,8 @@
 $routes->get("admin","\User\Controllers\Admin");
 $routes->group("user/auth",function($routes){
     $routes->add("login","\User\Controllers\Auth::login");
+    $routes->add("logout","\User\Controllers\Auth::logout");
+    $routes->add("change_password","\User\Controllers\Auth::change_password");
 
 })
 ?>
