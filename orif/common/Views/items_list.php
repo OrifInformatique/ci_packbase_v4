115a9c1755d8c917b42db24b9f21aeb33c277d2d
Didier Viret <didier.viret@orif.ch>
2021-03-03 11:34:10 +0100

    Display dynamic table content

--- orif/common/Views/items_list.php
+++ orif/common/Views/items_list.php
@@ -1,5 +1,5 @@
-<div class="list_auto container">
-    <h3><?= esc($list_title) ?></h3>
+<div class="items_list container">
+    <?= isset($list_title) ? '<h3>'.esc($list_title).'</h3>' : '' ?>
 
     <div class="row mb-2">
         <div class="col-sm-5 text-left">
@@ -15,25 +15,28 @@
         <table class="table table-striped table-hover">
             <thead>
                 <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Nom</th>
-                    <th scope="col">No inventaire</th>
-                    <th scope="col">Date d'achat</th>
-                    <th scope="col">Durée garantie</th>
+                    <!-- Get columns headers from the "columns" variable -->
+                    <?php foreach ($columns as $column): ?>
+                        <th scope="col"><?= esc($column) ?></th>
+                    <?php endforeach ?>
+
+                    <!-- Add the "action" column (for edit / delete / ...) -->
                     <th class="text-right" scope="col"></th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($items as $item): ?>
+                <!-- One table row for each item -->
+                <?php foreach ($items as $itemEntity): ?>
                 <tr>
-                    <td><?= esc($item['id']) ?></td>
-                    <td><a href="<?= site_url('stock/item_update/'.esc($item['id'])) ?>">
-                        <?= esc($item['name']) ?>
-                    </a></td>
-                    <td><?= esc($item['inventory_nb']) ?></td>
-                    <td><?= esc($item['buying_date']) ?></td>
-                    <td><?= esc($item['warranty_duration']) ?> mois</td>
-                    <td class="text-right"><a href="<?= site_url('stock/item_delete/'.esc($item['id'])) ?>">
+                    <!-- Only display item's properties wich are listed in "columns" variable -->
+                    <?php foreach ($itemEntity as $propertyKey => $propertyValue): 
+                        if (array_key_exists($propertyKey, $columns)) {
+                            echo ('<td>'.esc($propertyValue).'</td>');
+                        }
+                    endforeach ?>
+                    
+                    <!-- Add the "action" column (for edit / delete / ...) -->
+                    <td class="text-right"><a href="<?= site_url('stock/item_delete/'.esc($itemEntity['id'])) ?>">
                         <!-- Bootstrap Trash icon -->
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
--- orif/welcome/Controllers/Home.php
+++ orif/welcome/Controllers/Home.php
@@ -14,15 +14,19 @@ class Home extends BaseController
          * @todo TEST DATASET, TO BE REMOVED
          */
 		$data['list_title'] = "Test de la vue items_list";
+        $data['columns'] = ['name' => 'Nom',
+                            'inventory_nb' => 'No d\'inventaire',
+                            'buying_date' => 'date d\'achat',
+                            'warranty_duration' => 'durée de garantie'];
 
         $data['items'] = [
-            ['id' => '1', 'name' => 'Item 1', 'inventory_nb' => 'ITM0001', 'buying_date' => '01.01.2020', 'warranty_duration' => '12'],
-            ['id' => '2', 'name' => 'Item 2', 'inventory_nb' => 'ITM0002', 'buying_date' => '01.02.2020', 'warranty_duration' => '12'],
-            ['id' => '3', 'name' => 'Item 3', 'inventory_nb' => 'ITM0003', 'buying_date' => '01.03.2020', 'warranty_duration' => '12'],
-            ['id' => '4', 'name' => 'Item 4', 'inventory_nb' => 'ITM0004', 'buying_date' => '01.04.2020', 'warranty_duration' => '12'],
-            ['id' => '5', 'name' => 'Item 5', 'inventory_nb' => 'ITM0005', 'buying_date' => '01.05.2020', 'warranty_duration' => '12'],
-            ['id' => '6', 'name' => 'Item 6', 'inventory_nb' => 'ITM0006', 'buying_date' => '01.06.2020', 'warranty_duration' => '12'],
-            ['id' => '7', 'name' => 'Item 7', 'inventory_nb' => 'ITM0007', 'buying_date' => '01.07.2020', 'warranty_duration' => '12'],
+            ['id' => '1', 'name' => 'Item 1', 'inventory_nb' => 'ITM0001', 'buying_date' => '01.01.2020', 'warranty_duration' => '12 months'],
+            ['id' => '2', 'name' => 'Item 2', 'inventory_nb' => 'ITM0002', 'buying_date' => '01.02.2020', 'warranty_duration' => '12 months'],
+            ['id' => '3', 'name' => 'Item 3', 'inventory_nb' => 'ITM0003', 'buying_date' => '01.03.2020', 'warranty_duration' => '12 months'],
+            ['id' => '4', 'name' => 'Item 4', 'inventory_nb' => 'ITM0004', 'buying_date' => '01.04.2020', 'warranty_duration' => '12 months'],
+            ['id' => '5', 'name' => 'Item 5', 'inventory_nb' => 'ITM0005', 'buying_date' => '01.05.2020', 'warranty_duration' => '12 months'],
+            ['id' => '6', 'name' => 'Item 6', 'inventory_nb' => 'ITM0006', 'buying_date' => '01.06.2020', 'warranty_duration' => '12 months'],
+            ['id' => '7', 'name' => 'Item 7', 'inventory_nb' => 'ITM0007', 'buying_date' => '01.07.2020', 'warranty_duration' => '12 months'],
         ];
 
 		$this->display_view(['Common\Views\items_list','Welcome\welcome_message'], $data);
