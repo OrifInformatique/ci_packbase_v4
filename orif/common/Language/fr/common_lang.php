fbf967de06c742463ca06ac68bd534238c692b1e
maximeor <maximeeugene.herbez@formation.orif.ch>
2021-03-12 09:57:13 +0100

    Adding common text

--- orif/common/Language/fr/common_lang.php
+++ orif/common/Language/fr/common_lang.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * French translations for common module
- * 
+ *
  * @author      Orif
  * @link        https://github.com/OrifInformatique
  * @copyright   Copyright (c), Orif (http://www.orif.ch)
@@ -9,14 +9,22 @@
 
 return[
 // Application name
-'app_title'                      => 'Titre de l\'application',
+    'app_title'                      => 'Titre de l\'application',
 
 // Page titles
-'page_prefix'                    => 'Packbase V4',
+    'page_prefix'                    => 'Packbase V4',
 
 // Buttons
-'btn_admin'                      => 'Administration',
-'btn_login'                      => 'Se connecter',
-'btn_logout'                     => 'Se déconnecter',
-'btn_change_my_password'         => 'Modifier mon mot de passe'
+    'btn_admin'                      => 'Administration',
+    'btn_login'                      => 'Se connecter',
+    'btn_logout'                     => 'Se déconnecter',
+    'btn_change_my_password'         => 'Modifier mon mot de passe',
+    'btn_cancel'                     => 'Annuler',
+    'btn_save'                       => 'Enregistrer',
+    'btn_add_m'                      => 'Nouveau',
+    'btn_add_f'                      => 'Nouvelle',
+
+// Text
+    'yes'                            => 'Oui',
+    'no'                             => 'Non'
 ];
