b6fa41356257c2291aed6843d12c3aca464c05be
maximeor <maximeeugene.herbez@formation.orif.ch>
2021-03-12 09:55:43 +0100

    Updated BaseController.php

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -5,7 +5,13 @@ namespace App\Controllers;
 use CodeIgniter\Controller;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Model;
+use CodeIgniter\Session\Session;
+use CodeIgniter\Validation\Validation;
 use Psr\Log\LoggerInterface;
+use User\Controllers\Auth;
+use User\Models\User_model;
+use User\Models\User_type_model;
 
 /**
  * Class BaseController
@@ -20,65 +26,52 @@ use Psr\Log\LoggerInterface;
 
 class BaseController extends Controller
 {
-	/**
-	 * An array of helpers to be loaded automatically upon
-	 * class instantiation. These helpers will be available
-	 * to all other controllers that extend BaseController.
-	 *
-	 * @var array
-	 */
-	protected $helpers = [];
-	/** add access level BaseController like in v3*/
-    protected $access_level = "*";
-	/**
-	 * Constructor.
-	 *
-	 * @param RequestInterface  $request
-	 * @param ResponseInterface $response
-	 * @param LoggerInterface   $logger
-	 */
-	public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
-	{
-	    if (!$this->check_permission()){
-            //throw new \Exception();
-        }
-
-
-		// Do Not Edit This Line
-		parent::initController($request, $response, $logger);
-
-		//--------------------------------------------------------------------
-		// Preload any models, libraries, etc, here.
-		//--------------------------------------------------------------------
-		// E.g.: $this->session = \Config\Services::session();
-	}
     /**
-     * Check if user access level matches the required access level.
-     * Required level can be the controller's default level or a custom
-     * specified level.
+     * An array of helpers to be loaded automatically upon
+     * class instantiation. These helpers will be available
+     * to all other controllers that extend BaseController.
      *
-     * @param  $required_level : minimum level required to get permission
-     * @return bool : true if user level is equal or higher than required level,
-     *                false else
+     * @var array
      */
-    protected function check_permission($required_level = NULL)
+    protected $helpers = [];
+    /** add access level BaseController like in v3*/
+    protected $access_level = "*";
+    protected Validation $validation;
+    protected Session $session;
+    protected Model $user_model;
+    protected Model $user_type_model;
+    /**
+     * Constructor.
+     *
+     * @param RequestInterface  $request
+     * @param ResponseInterface $response
+     * @param LoggerInterface   $logger
+     */
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
-        if ($required_level == "*") {
-            // page is accessible for all users
-            return true;
-        }
-        else {
 
-        }
+
+
+        // Do Not Edit This Line
+        parent::initController($request, $response, $logger);
+        //--------------------------------------------------------------------
+        // Preload any models, libraries, etc, here.
+        //--------------------------------------------------------------------
+        // E.g.: $this->session = \Config\Services::session();
+        $this->session = \Config\Services::session();
+        $this->validation=\Config\Services::validation();
+        $this->user_model=new User_model();
+        $this->user_type_model=new User_type_model();
+
     }
 
-	/**
-    * Display one or multiple view(s), adding header, footer and
-	* any other view part wich is common to all pages.
-    *
-    * @param  $view_parts : single view or array of view parts to display
-    *         $data : data array to send to the view
-    */
+    /**
+     * Display one or multiple view(s), adding header, footer and
+     * any other view part wich is common to all pages.
+     *
+     * @param  $view_parts : single view or array of view parts to display
+     *         $data : data array to send to the view
+     */
     public function display_view($view_parts, $data = NULL)
     {
         // If not defined in $data, set page title to empty string
@@ -88,7 +81,7 @@ class BaseController extends Controller
 
         // Display common headers
         echo view('Common\header', $data);
-	    echo view('Common\login_bar');
+        echo view('Common\login_bar');
 
         if (is_array($view_parts)) {
             // Display multiple view parts
