8eb344d6daa6993b9db4f71a847797f3ddb74718
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2021-03-15 09:34:09 +0100

    Added routes to Routes.php

--- orif/user/Config/Routes.php
+++ orif/user/Config/Routes.php
@@ -1,14 +1,14 @@
 <?php
 $routes->group("user/auth",function($routes){
-    $routes->add("","\User\Controllers\Auth::login");
     $routes->add("login","\User\Controllers\Auth::login");
     $routes->add("logout","\User\Controllers\Auth::logout");
     $routes->add("change_password","\User\Controllers\Auth::change_password");
 });
 $routes->group("user/admin",function($routes){
-    $routes->add("","\User\Controllers\Admin::index");
-    $routes->add("list_user","\User\Controllers\Admin::list_user",['filter'=>"logfilter:{config('\User\Config\UserConfig')->access_lvl_admin}"]);
-    $routes->add("list_user/(:num)","\User\Controllers\Admin::list_user/$1",['filter'=>"logfilter:{config('\User\Config\UserConfig')->access_lvl_admin}"]);
-    $routes->add("save_user/(:num)","\User\Controllers\Admin::list_user/$1",['filter'=>"logfilter:{config('\User\Config\UserConfig')->access_lvl_admin}"]);
+   $routes->add("list_user","\User\Controllers\Admin::list_user",['filter'=>"logfilter:{config('\User\Config\User_config')->access_lvl_admin}"]);
+   $routes->add("list_user/(:num)","\User\Controllers\Admin::list_user/$1",['filter'=>"logfilter:{config('\User\Config\User_config')->access_lvl_admin}"]);
+   $routes->add("save_user/(:num)","\User\Controllers\Admin::save_user/$1",['filter'=>"logfilter:{config('\User\Config\User_config')->access_lvl_admin}"]);
+   $routes->add("save_user","\User\Controllers\Admin::save_user",['filter'=>"logfilter:{config('\User\Config\User_config')->access_lvl_admin}"]);
+
 });
 ?>
