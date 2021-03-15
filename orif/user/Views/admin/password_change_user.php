b376680bc85838a11c7049299b7cb048dcebbfaa
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2021-03-15 11:51:37 +0100

    updated view password_change_user.php

--- orif/user/Views/admin/password_change_user.php
+++ orif/user/Views/admin/password_change_user.php
@@ -1,4 +1,5 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+$validation=\Config\Services::validation();
 // Required for config values
 ?>
 <div class="container">
@@ -7,21 +8,21 @@
             'id' => 'user_change_password_form',
             'name' => 'user_change_password_form'
         );
-        echo form_open('user/admin/password_change_user/'.$user->id, $attributes);
+        echo form_open('user/admin/password_change_user/'.$user['id'], $attributes);
     ?>
     
     <!-- TITLE -->
     <div class="row">
         <div class="col-12">
-            <h1 class="title-section"><?= lang('title_user_password_reset'); ?></h1>
-            <h4><?= lang('user')." : ".$user->username ?></h4>
+            <h1 class="title-section"><?= lang('MY_user_lang.title_user_password_reset'); ?></h1>
+            <h4><?= lang('MY_user_lang.user')." : ".$user['username'] ?></h4>
         </div>
     </div>
     
     <!-- ERRORS -->
     <div class="row">
         <div class="col-12">
-            <?= validation_errors('<div class="alert alert-danger">', '</div>'); ?>
+            <?= $validation->listErrors('user_error_list'); ?>
         </div>
     </div>
     
@@ -29,19 +30,19 @@
     <div class="row">
         <div class="col-sm-6">
             <div class="form-group">
-                <?= form_label(lang('field_new_password'), 'user_password_new', ['class' => 'form-label']); ?>
+                <?= form_label(lang('MY_user_lang.field_new_password'), 'user_password_new', ['class' => 'form-label']); ?>
                 <?= form_password('user_password_new', '', [
                     'class' => 'form-control', 'id' => 'user_password_new',
-                    'maxlength' => $this->config->item('password_max_length')
+                    'maxlength' => config('\User\Config\UserConfig')->password_max_length
                 ]); ?>
             </div>
         </div>
         <div class="col-sm-6">
             <div class="form-group">
-                <?= form_label(lang('field_password_confirm'), 'user_password_again', ['class' => 'form-label']); ?>
+                <?= form_label(lang('MY_user_lang.field_password_confirm'), 'user_password_again', ['class' => 'form-label']); ?>
                 <?= form_password('user_password_again', '', [
                     'class' => 'form-control', 'id' => 'user_password_new',
-                    'maxlength' => $this->config->item('password_max_length')
+                    'maxlength' => config('\User\Config\UserConfig')->password_max_length
                 ]); ?>
             </div>
         </div>
@@ -50,9 +51,9 @@
     <!-- SUBMIT / CANCEL -->
     <div class="row">
         <div class="col-12 text-right">
-            <a name="cancel" class="btn btn-default" href="<?= base_url('user/admin/list_user'); ?>"><?= lang('btn_cancel'); ?></a>
+            <a name="cancel" class="btn btn-default" href="<?= base_url('user/admin/list_user'); ?>"><?= lang('MY_user_lang.btn_cancel'); ?></a>
             &nbsp;
-            <?= form_submit('save', lang('btn_save'), ['class' => 'btn btn-primary']); ?>
+            <?= form_submit('save', lang('MY_user_lang.btn_save'), ['class' => 'btn btn-primary']); ?>
         </div>
     </div>
     <?= form_close(); ?>
