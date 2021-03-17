84acdbd5318e705276a6a81840cc8ffb9effc6aa
Didier Viret <didier.viret@orif.ch>
2021-03-17 11:49:53 +0100

    Adapt readme.md file

--- orif/common/Views/delete_confirm.php
+++
@@ -1,8 +0,0 @@
-<h2><?= esc($title) ?></h2>
-<p><strong>Nom : </strong><?= esc($item['name']) ?></p>
-<p><strong>No inventaire : </strong><?= esc($item['inventory_nb']) ?></p>
-<br>
-<p>
-    <a href="<?= site_url('stock/item_delete/'.$item['id'].'/confirmed') ?>">Supprimer</a>&nbsp;&nbsp;&nbsp;
-    <a href="<?= site_url('stock/items_list') ?>">Annuler</a>
-</p>
