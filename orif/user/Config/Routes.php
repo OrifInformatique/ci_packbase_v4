e980bfcb9a245513985daec936d522dda28f1f00
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2021-03-17 13:18:57 +0100

    removed filters

--- orif/user/Config/Routes.php
+++ orif/user/Config/Routes.php
@@ -5,14 +5,14 @@ $routes->group("user/auth",function($routes){
     $routes->add("change_password","\User\Controllers\Auth::change_password");
 });
 $routes->group("user/admin",function($routes){
-   $routes->add("list_user","\User\Controllers\Admin::list_user",['filter'=>"logfilter:{config('\User\Config\User_config')->access_lvl_admin}"]);
-   $routes->add("list_user/(:num)","\User\Controllers\Admin::list_user/$1",['filter'=>"logfilter:{config('\User\Config\User_config')->access_lvl_admin}"]);
-   $routes->add("save_user/(:num)","\User\Controllers\Admin::save_user/$1",['filter'=>"logfilter:{config('\User\Config\User_config')->access_lvl_admin}"]);
-   $routes->add("save_user","\User\Controllers\Admin::save_user",['filter'=>"logfilter:{config('\User\Config\User_config')->access_lvl_admin}"]);
-   $routes->add("delete_user/(:num)","\User\Controllers\Admin::delete_user/$1",['filter'=>"logfilter:{config('\User\Config\User_config')->access_lvl_admin}"]);
-   $routes->add("delete_user/(:num)/(:num)","\User\Controllers\Admin::delete_user/$1/$2",['filter'=>"logfilter:{config('\User\Config\User_config')->access_lvl_admin}"]);
-   $routes->add("reactivate_user/(:num)","\User\Controllers\Admin::reactivate_user/$1",['filter'=>"logfilter:{config('\User\Config\User_config')->access_lvl_admin}"]);
-   $routes->add("password_change_user/(:num)","\User\Controllers\Admin::password_change_user/$1",['filter'=>"logfilter:{config('\User\Config\User_config')->access_lvl_admin}"]);
+   $routes->add("list_user","\User\Controllers\Admin::list_user");
+   $routes->add("list_user/(:num)","\User\Controllers\Admin::list_user/$1");
+   $routes->add("save_user/(:num)","\User\Controllers\Admin::save_user/$1");
+   $routes->add("save_user","\User\Controllers\Admin::save_user");
+   $routes->add("delete_user/(:num)","\User\Controllers\Admin::delete_user/$1");
+   $routes->add("delete_user/(:num)/(:num)","\User\Controllers\Admin::delete_user/$1/$2");
+   $routes->add("reactivate_user/(:num)","\User\Controllers\Admin::reactivate_user/$1");
+   $routes->add("password_change_user/(:num)","\User\Controllers\Admin::password_change_user/$1");
 
 });
 ?>
