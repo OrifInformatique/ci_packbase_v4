4d3f5add83f893499be85f870cf893d29f4a8614
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2021-03-17 13:13:43 +0100

    added 403 error page and language

---
+++ orif/user/Language/fr/error_language.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * error_language
+ *
+ * @author      Orif (ViDi)
+ * @link        https://github.com/OrifInformatique
+ * @copyright   Copyright (c), Orif (https://www.orif.ch)
+ */
+/**
+ * Translation for module errors
+ */
+return[
+// Error messages
+    'msg_err_access_forbidden'  => 'Vous n\'avez pas les droits requis pour accéder à cette page',
+// text
+    '403_error'                     => 'Erreur 403',
+];
+?>
---
+++ orif/user/Views/errors/403error.php
@@ -0,0 +1,21 @@
+<?php
+http_response_code(403);
+?>
+<style>
+    #message{
+        width: 50vw!important;
+        margin-left: 25%;
+        height: 200px;
+        border-radius: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: x-large;
+        margin-top: 20px;
+    }
+</style>
+<div style="width: 100%;background-color: #106DB4;color: white;margin: 0;padding: 0;height: 15vh;display: flex;justify-content: center;align-items: center"><h1 style="margin: 0;padding: 0"><?= lang('error_language.403_error')?></h1></center></div>
+<div id="corper">
+    <p id="message" class="alert-danger text-danger" style="display:flex;justify-content:center;width: 100%" onload="animate"><?= lang('error_language.msg_err_access_forbidden')?></p>
+    <a href="<?=$_SESSION['_ci_previous_url']?>" style="padding-left: 27%"><?=lang('My_user_lang.btn_back');?></a>
+</div>
