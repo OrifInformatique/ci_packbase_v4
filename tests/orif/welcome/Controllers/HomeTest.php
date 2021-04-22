b18ae9a738bd8f22e052190754f5c62d1fae5b1e
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2021-04-22 08:22:11 +0200

    Added test cdn link and testTags

--- tests/orif/welcome/Controllers/HomeTest.php
+++ tests/orif/welcome/Controllers/HomeTest.php
@@ -11,14 +11,35 @@ class HomeTest extends CIUnitTestCase
 
     public function testIndexView()
     {
-        $results = $this->withURI(base_url())
+        $resultsofhome = $this->withURI(base_url())
                         ->controller(\Welcome\Controllers\Home::class)
                         ->execute('index');
 
         // Test if result is ok and verify that all view parts are present
-        $this->assertTrue($results->isOK());
-        $this->assertTrue($results->see('charset="utf-8"'));
-        $this->assertTrue($results->see(lang('common_lang.app_title')));
-        $this->assertTrue($results->see('Welcome to CodeIgniter'));
+        $this->assertTrue($resultsofhome->isOK());
+        $this->assertTrue($resultsofhome->see('charset="utf-8"'));
+        $this->assertTrue($resultsofhome->see(lang('common_lang.app_title')));
+        $this->assertTrue($resultsofhome->see('Welcome to CodeIgniter'));
     }
+    public function testLinksCDN(){
+        $client =\Config\Services::curlrequest();
+        //teste jquery
+        $this->assertTrue($client->request('GET','https://code.jquery.com/jquery-3.4.1.min.js')->getStatusCode()==200);
+        //teste popper
+        $this->assertTrue($client->request('GET','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js')->getStatusCode()==200);
+        //teste bootstrapjs
+        $this->assertTrue($client->request('GET','https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js')->getStatusCode()==200);
+    }
+    public function testTags(){
+        $client =\Config\Services::curlrequest();
+        $response=$client->request('GET',base_url())->getBody();
+        $this->assertTrue(\strpos($response,'<head>')&&strpos($response,'</head>'));
+        $this->assertTrue(\strpos($response,'<body>')&&strpos($response,'</body>'));
+        $this->assertTrue(\strpos($response,'<html>')&&strpos($response,'</html>'));
+    } 
+    public function testUser(){
+        $client =\Config\Services::curlrequest();
+        
+    }
+
 }
