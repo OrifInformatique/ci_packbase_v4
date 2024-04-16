f0cde874da76baa442b396118dbab25fd1d482bf
ovsiankina <ovsiankina@proton.me>
2024-04-16 11:29:25 +0200

    Test_bidon reverted

--- tests/orif/common/Views/AdminMenuTest.php
+++ tests/orif/common/Views/AdminMenuTest.php
@@ -27,25 +27,18 @@ class AdminMenuTest extends CIUnitTestCase
         parent::tearDown();
 
     }
-    
-    public function test_bidon(){
-      $x = 1;
-      $y = 1;
-      
-      $this->assertEquals($x, $y);
+
+    public function test_panel_config_with_administrator_session() 
+    {
+        $_SESSION['logged_in'] = true;
+        $_SESSION['user_access'] = Config('\User\Config\UserConfig')
+           ->access_lvl_admin;
+        $_SESSION['_ci_previous_url'] = 'url';
+
+        $result = $this->withSession()->get('user/admin/list_user');
+        // Assertions
+        $response = $result->response();
+        $body = $response->getBody();
+        $result->assertSee(lang('user_lang.title_user_list'), 'h1');
     }
-
-    // public function test_panel_config_with_administrator_session() 
-    // {
-    //     $_SESSION['logged_in'] = true;
-    //     $_SESSION['user_access'] = Config('\User\Config\UserConfig')
-    //        ->access_lvl_admin;
-    //     $_SESSION['_ci_previous_url'] = 'url';
-
-    //     $result = $this->withSession()->get('user/admin/list_user');
-    //     // Assertions
-    //     $response = $result->response();
-    //     $body = $response->getBody();
-    //     $result->assertSee(lang('user_lang.title_user_list'), 'h1');
-    // }
 }
