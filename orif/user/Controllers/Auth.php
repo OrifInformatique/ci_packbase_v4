6999e7b862dd49b0366bb56d6bf7bcb17c2ea5aa
Didier Viret <didier.viret@orif.ch>
2024-06-14 11:15:37 +0200

    Move e-mail texts in language files

--- orif/user/Controllers/Auth.php
+++ orif/user/Controllers/Auth.php
@@ -354,10 +354,10 @@ use User\Controllers\Profile;
         $appTitle = lang('common_lang.app_title');
         $email->initialize($emailConfig);
         //Sending code to user's orif 
-        $email->setFrom('verif@sectioninformatique.ch', 'ORIF: Vérification du mail');
+        $email->setFrom(getenv('SMTP_ID'), lang('common_lang.app_title'));
         // 2nd paramater hard coded since variable not interpreted in SetFrom$email->setTo($form_email);
-        $email->setSubject('Code de vérification');
-        $email->setMessage('Voici votre code de vérification: '.$verification_code);
+        $email->setSubject(lang('user_lang.mail_verification_code_subject'));
+        $email->setMessage(lang('user_lang.mail_verification_code_text').$verification_code);
         $email->send(false);
 
         // Set code's expiration timer
