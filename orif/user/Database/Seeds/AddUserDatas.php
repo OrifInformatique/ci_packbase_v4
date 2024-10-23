eea4334881fec23b0f920c00c6ad08d6c63bb4cb
Didier Viret <didier.viret@orif.ch>
2024-10-23 13:12:57 +0200

    Complete README.md canvas

--- orif/user/Database/Seeds/AddUserDatas.php
+++ orif/user/Database/Seeds/AddUserDatas.php
@@ -9,8 +9,8 @@ class AddUserDatas extends \CodeIgniter\Database\Seeder
     public function run()
     {
         $data = [
-            ['fk_user_type'=>1,'username'=>'admin','password'=>'$2y$10$84r63xo.M4LVcIi8IvT8cO0qYxyglPshY1jJmKLedRMcaTcxhcVYO'],
-            ['fk_user_type'=>2,'username'=>'utilisateur','password'=>'$2y$10$11wIuR3FnfWwTpfyJ9WCz.E3KErvb.i.Q2Wef6XMUZHTXUlW0FhJm']
+            ['fk_user_type'=>1,'username'=>'admin','email'=>'admin@test.com','password'=>'$2y$10$uvix1BJUvu.acqlaNlsmieKlgWuYA7/MfwdHeSFjwvdeqIZtO/Utm'],
+            ['fk_user_type'=>2,'username'=>'user','email'=>'user@test.com','password'=>'$2y$10$hrihONbH2MT.eh6MGkSZ5ekADbCGFBUGHz5YsXIFI3W4QPh.2N16W']
         ];
         foreach($data as $row)
         $this->db->table('user')->insert($row);
